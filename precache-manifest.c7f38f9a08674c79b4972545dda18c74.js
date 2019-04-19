1111648c6af6f9e4740dbedcf06e4d84533e6abe
Ritesh Jagga <riteshjagga@gmail.com>
2019-04-19 18:35:33 +0530

    Updates

---
+++ precache-manifest.c7f38f9a08674c79b4972545dda18c74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  },
+  {
+    "revision": "666c3432d24ab1410e76",
+    "url": "/react-tutorial/static/js/main.65b9babd.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-tutorial/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "666c3432d24ab1410e76",
+    "url": "/react-tutorial/static/css/main.fc537a48.chunk.css"
+  },
+  {
+    "revision": "ba5ce56087dd81404cca5da839d629e2",
+    "url": "/react-tutorial/index.html"
+  }
+];
